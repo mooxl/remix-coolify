fe68dd95e8963e02284134a2b4a2ff5fac65b447
Max Schmidt <max.schmidt@outlook.de>
2024-03-16 20:26:18 +0100

    initial commit
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

---
+++ app/routes/_index.tsx
@@ -0,0 +1,16 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+	return [
+		{ title: "New Remix App" },
+		{ name: "description", content: "Welcome to Remix!" },
+	];
+};
+
+export default function Index() {
+	return (
+		<main>
+			<h1>This is world</h1>
+		</main>
+	);
+}
