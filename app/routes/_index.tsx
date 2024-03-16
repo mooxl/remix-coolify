b0bdfc83f838adf6593b21ff6f447e4c9e9f82c6
Max Schmidt <max.schmidt@outlook.de>
2024-03-16 20:57:31 +0100

    lets check
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,16 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { HeadersFunction, MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
 	return [
 		{ title: "New Remix App" },
 		{ name: "description", content: "Welcome to Remix!" },
 	];
+};
+
+export const headers: HeadersFunction = () => {
+	return {
+		"cache-control": "public, max-age=1, stale-while-revalidate=8640000",
+	};
 };
 
 export default function Index() {
