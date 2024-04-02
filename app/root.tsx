6ec9960527e4a3b84f82370d5a1b15b76644f4ab
Max Schmidt <max.schmidt@outlook.de>
2024-04-02 18:31:47 +0200

    heya
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- app/root.tsx
+++ app/root.tsx
@@ -1,38 +1,52 @@
-import { HeadersFunction } from "@remix-run/node";
+import type {
+  HeadersFunction,
+  LinksFunction,
+  MetaFunction,
+} from "@remix-run/node";
 import {
-	Links,
-	Meta,
-	Outlet,
-	Scripts,
-	ScrollRestoration,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
 } from "@remix-run/react";
-
 import "./global.css";
 
 export const headers: HeadersFunction = () => {
-	return {
-		"cache-control": "public, max-age=1, stale-while-revalidate=864000",
-	};
+  return {
+    "cache-control": "public, max-age=1, stale-while-revalidate=864000",
+  };
 };
 
-export function Layout({ children }: { children: React.ReactNode }) {
-	return (
-		<html lang="de">
-			<head>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<Meta />
-				<Links />
-			</head>
-			<body>
-				{children}
-				<ScrollRestoration />
-				<Scripts />
-			</body>
-		</html>
-	);
+export const links: LinksFunction = () => {
+  return [{ rel: "icon", href: "favicon.svg", type: "image/svg+xml" }];
+};
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+const Root =()=> {
+  return (
+    <html lang="de">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="">
+        <main className="">
+          <Outlet />
+        </main>
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
 }
 
-export default function App() {
-	return <Outlet />;
-}
+export default Root;
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,30 +1,9 @@
-import Image from "@/components/Image";
-import type { MetaFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-
-export const meta: MetaFunction = () => {
-	return [
-		{ title: "New Remix App" },
-		{ name: "description", content: "Welcome to Remix!" },
-	];
+const Page = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
 };
 
-export const loader = async () => {
-	const date = new Date().toISOString().split("T")[1].split(".")[0];
-	//await new Promise((resolve) => setTimeout(resolve, 10000));
-	return { date };
-};
-
-export default function Index() {
-	const data = useLoaderData<typeof loader>();
-	return (
-		<main>
-			<h1 className="">Server creation - {data.date}</h1>
-			<Image
-				src="https://cms.mooxl.de/assets/f0d200c6-0e9a-4807-a3e9-f01c1cbf333c"
-				alt="A cat with a hat"
-			/>
-			<h2>Test</h2>
-		</main>
-	);
-}
+export default Page;
